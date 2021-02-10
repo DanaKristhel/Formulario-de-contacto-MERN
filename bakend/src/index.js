d880972181b8b7a51ec1f4fdd74653743c1a5a5a
danak <danakristhel@gmail.com>
2021-02-10 12:20:40 -0300

    initializing port

---
+++ bakend/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyparser = require('body-parser')
+
+app.set('port', 4000)
+
+//app.use(morgan('dev'))
+
+//app.use(bodyparser.urlencoded({extended:true}))
+//app.use(bodyparser.json())
+
+//start server
+
+app.listen(app.get('port'),()=>{
+    console.log('listen port ', app.get('port'))
+})
